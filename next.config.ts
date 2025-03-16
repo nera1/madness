37970a7c474ea93d1311e6c8f2a41f6910357e9d
nera1 <nera4936@gmail.com>
2025-03-16 23:47:20 +0900

    feat: update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
+const debug = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
   output: "export",
+  reactStrictMode: false,
+  assetPrefix: debug ? `/` : "",
+  basePath: debug ? `` : "",
+  trailingSlash: true,
 };
 
 export default nextConfig;
