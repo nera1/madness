38d0ffa03d858eed8a2bbc13ad51d417d25326c8
nera1 <nera4936@gmail.com>
2025-04-16 23:57:02 +0900

    feat: add inputs in signup page

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -3,6 +3,8 @@ import Header from "@/components/header/header";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 
+import { Button } from "@/components/ui/button";
+
 import styles from "@/styles/signup.module.scss";
 
 export default function Signup() {
@@ -10,13 +12,32 @@ export default function Signup() {
     <>
       <Header logoCenter />
       <main className={`${styles["signup"]} flex justify-center`}>
-        <div className={`${styles["container"]} pt-3 text-white`}>
-          <h1 className="text-2xl font-extrabold tracking-tight">Sign up</h1>
-          <form>
-            <div className="grid w-full max-w-sm items-center gap-1.5">
+        <div className={`${styles["container"]} flex flex-col align-center`}>
+          <h1 className="text-3xl font-extrabold tracking-tight">Sign up</h1>
+          <form className="flex justify-center flex-col align-center gap-y-5 py-4">
+            <div className="flex flex-col gap-2">
               <Label htmlFor="email">Email</Label>
               <Input type="email" id="email" placeholder="Email" />
             </div>
+            <div className="flex flex-col gap-2">
+              <Label htmlFor="nickname">Nickname</Label>
+              <Input type="text" id="nicname" placeholder="Nickname" />
+            </div>
+            <div className="flex flex-col gap-2">
+              <Label htmlFor="password">Password</Label>
+              <Input type="password" id="password" placeholder="Password" />
+            </div>
+            <div className="flex flex-col gap-2">
+              <Label htmlFor="confirm-password">Confirm password</Label>
+              <Input
+                type="password"
+                id="confirm-password"
+                placeholder="Confirm password"
+              />
+            </div>
+            <Button type="submit" className="cursor-pointer">
+              Sign up
+            </Button>
           </form>
         </div>
       </main>
