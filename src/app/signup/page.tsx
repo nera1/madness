8db7a5eb58e3dddd8879d09dc50695e2beacc7ef
nera1 <nera4936@gmail.com>
2025-05-04 23:59:48 +0900

    feat:add handlers

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,14 +1,11 @@
 "use client";
 
 import Header from "@/components/header/header";
-
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-
 import { Button } from "@/components/ui/button";
-
 import styles from "@/styles/signup.module.scss";
-import { useState } from "react";
+import { ChangeEventHandler, useState } from "react";
 
 type SignupState = {
   email: string;
@@ -18,12 +15,74 @@ type SignupState = {
 };
 
 export default function Signup() {
-  const [state] = useState<SignupState>({
+  const [state, setState] = useState<SignupState>({
     email: "",
     nickname: "",
     password: "",
     confirmPassword: "",
   });
+  const [errors, setErrors] = useState<SignupState>({
+    email: "",
+    nickname: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  const passwordRegex =
+    /^(?=.*[A-Za-z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{8,}$/;
+
+  const onChangeEmail: ChangeEventHandler<HTMLInputElement> = (event) => {
+    const email = event.target.value;
+    setState({ ...state, email });
+    setErrors({
+      ...errors,
+      email: emailRegex.test(email) ? "" : "Invalid email format",
+    });
+  };
+
+  const onChangeNickname: ChangeEventHandler<HTMLInputElement> = (event) => {
+    setState({ ...state, nickname: event.target.value });
+  };
+
+  const onChangePassword: ChangeEventHandler<HTMLInputElement> = (event) => {
+    const password = event.target.value;
+    setState({ ...state, password });
+    setErrors({
+      ...errors,
+      password: passwordRegex.test(password)
+        ? ""
+        : "Password must be at least 8 characters, include a letter, a number, and a special character.",
+    });
+  };
+
+  const onChangeConfirmPassword: ChangeEventHandler<HTMLInputElement> = (
+    event
+  ) => {
+    const confirmPassword = event.target.value;
+    setState({ ...state, confirmPassword });
+    setErrors({
+      ...errors,
+      confirmPassword:
+        confirmPassword === state.password ? "" : "Passwords do not match",
+    });
+  };
+
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    // 추가적인 유효성 검사 및 폼 제출 처리
+    if (
+      !errors.email &&
+      !errors.password &&
+      !errors.confirmPassword &&
+      state.email &&
+      state.password &&
+      state.confirmPassword
+    ) {
+      // 폼 제출 로직
+      console.log("Form Submitted:", state);
+    }
+  };
 
   return (
     <>
@@ -38,14 +97,18 @@ export default function Signup() {
           <p className="text-base text-muted-foreground">
             Create your account and get started.
           </p>
-          <form className="flex justify-center flex-col align-center gap-y-5 py-8">
+          <form
+            className="flex justify-center flex-col align-center gap-y-5 py-8"
+            onSubmit={handleSubmit}
+          >
             <div className="flex flex-col gap-2">
               <Label htmlFor="email">Email</Label>
               <Input
                 type="email"
                 id="email"
-                placeholder="Email"
+                placeholder="Enter a valid email (e.g., user@example.com)"
                 value={state.email}
+                onChange={onChangeEmail}
               />
             </div>
             <div className="flex flex-col gap-2">
@@ -53,8 +116,9 @@ export default function Signup() {
               <Input
                 type="text"
                 id="nickname"
-                placeholder="Nickname"
+                placeholder="Enter a nickname"
                 value={state.nickname}
+                onChange={onChangeNickname}
               />
             </div>
             <div className="flex flex-col gap-2">
@@ -62,8 +126,9 @@ export default function Signup() {
               <Input
                 type="password"
                 id="password"
-                placeholder="Password"
+                placeholder="At least 8 characters, with letters, numbers, and symbols"
                 value={state.password}
+                onChange={onChangePassword}
               />
             </div>
             <div className="flex flex-col gap-2">
@@ -71,8 +136,9 @@ export default function Signup() {
               <Input
                 type="password"
                 id="confirm-password"
-                placeholder="Confirm password"
+                placeholder="Confirm your password"
                 value={state.confirmPassword}
+                onChange={onChangeConfirmPassword}
               />
             </div>
             <Button type="submit" className="cursor-pointer">
