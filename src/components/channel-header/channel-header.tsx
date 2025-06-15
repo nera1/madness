13e17835355ee4206507df75c786b8517f77f959
nera1 <nera4936@gmail.com>
2025-06-15 23:59:52 +0900

    fix: remove unused hook

--- src/components/channel-header/channel-header.tsx
+++ src/components/channel-header/channel-header.tsx
@@ -1,15 +1,10 @@
 "use client";
 
-import { useSearchParams } from "next/navigation";
-import { FunctionComponent, useEffect } from "react";
+import { FunctionComponent } from "react";
 
 import styles from "@/styles/channel-header.module.scss";
 
 const ChannelHeader: FunctionComponent = () => {
-  const searchParams = useSearchParams();
-  useEffect(() => {
-    console.log("Query param:", searchParams.get("c"));
-  }, [searchParams]);
   return (
     <header
       className={`${styles["channel-header"]} box-border flex justify-center`}
