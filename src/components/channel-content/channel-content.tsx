5a6dcbf52a19a093c88fa6a8a8d9f58f57b06513
nera1 <nera4936@gmail.com>
2025-06-17 18:36:39 +0900

    WIP: making chat ui

--- src/components/channel-content/channel-content.tsx
+++ src/components/channel-content/channel-content.tsx
@@ -2,17 +2,26 @@
 
 import { FunctionComponent, useEffect } from "react";
 import { useSearchParams } from "next/navigation";
+
 import ChannelHeader from "@/components/channel-header/channel-header";
+import styles from "@/styles/channel-content.module.scss";
 
 const ChannelContent: FunctionComponent = () => {
   const searchParams = useSearchParams();
+
   useEffect(() => {
     console.log("c:", searchParams.get("c"));
   }, [searchParams]);
+
   return (
     <>
       <ChannelHeader />
-      <main></main>
+      <main className={`${styles["channel-content"]} flex justify-center`}>
+        <div className={styles["container"]}>
+          <div className={styles["content"]}></div>
+          <div className={`${styles["input-area"]}`}></div>
+        </div>
+      </main>
     </>
   );
 };
