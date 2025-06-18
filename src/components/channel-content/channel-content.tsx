0e3ae7bf77c7e6ca8adcf8f7d094f7f4a58fb81c
nera1 <nera4936@gmail.com>
2025-06-18 18:25:56 +0900

    WIP: making channel page

--- src/components/channel-content/channel-content.tsx
+++ src/components/channel-content/channel-content.tsx
@@ -4,8 +4,12 @@ import { FunctionComponent, useEffect } from "react";
 import { useSearchParams } from "next/navigation";
 
 import ChannelHeader from "@/components/channel-header/channel-header";
+import { Button } from "../ui/button";
+
+import MadIcon from "../logo/MadIcon";
+import { Sticker } from "lucide-react";
+
 import styles from "@/styles/channel-content.module.scss";
-import { Input } from "../ui/input";
 
 const ChannelContent: FunctionComponent = () => {
   const searchParams = useSearchParams();
@@ -19,12 +23,34 @@ const ChannelContent: FunctionComponent = () => {
       <ChannelHeader />
       <main className={`${styles["channel-content"]} flex justify-center`}>
         <div className={styles["container"]}>
-          <div className={styles["content"]}></div>
-          <div className={`${styles["input-area"]} flex items-center`}>
-            <Input
-              type="text"
-              className="focus-visible:ring-0 border-0 rounded-sm rounded-md"
+          <div className={styles["content"]}>
+            <div className={`${styles["chat-menu"]}`}></div>
+            <ul
+              className={`${styles["chat-list"]} m-0 px-2 py-2 w-full h-full`}
+            ></ul>
+          </div>
+          <div className={`${styles["input-area"]} flex items-center gap-x-2`}>
+            <div>
+              <Button
+                size="icon"
+                className={`${styles["sticker-btn"]} hover:bg-neutral-800 size-9 cursor-pointer rounded-full [&_svg]:!h-5 [&_svg]:!w-5`}
+              >
+                <Sticker />
+              </Button>
+            </div>
+            <textarea
+              id="chat-input"
+              cols={0}
+              className="w-full px-3 pt-1 pb-2 h-9 box-border rounded-md resize-none"
             />
+            <div>
+              <Button
+                size="icon"
+                className={`${styles["submit-btn"]} size-9 [&_svg]:!h-6 [&_svg]:!w-6 cursor-pointer`}
+              >
+                <MadIcon fillColor="#000" bgColor="transparent" />
+              </Button>
+            </div>
           </div>
         </div>
       </main>
--- src/components/channel-header/channel-header.tsx
+++ src/components/channel-header/channel-header.tsx
@@ -2,16 +2,46 @@
 
 import { FunctionComponent } from "react";
 
+import { Button } from "../ui/button";
+
+import { ChevronLeft, Menu } from "lucide-react";
+
 import styles from "@/styles/channel-header.module.scss";
 
-const ChannelHeader: FunctionComponent = () => {
+interface ChannelHeaderProps {
+  name?: string;
+  creator?: string;
+}
+
+const ChannelHeader: FunctionComponent<ChannelHeaderProps> = () => {
   return (
     <header
       className={`${styles["channel-header"]} box-border flex justify-center`}
     >
-      <div className={`${styles["container"]} h-full flex`}>
-        <div className={`${styles["left"]} grow`}></div>
-        <div className={`${styles["right"]} grow`}></div>
+      <div className={`${styles["container"]} h-full flex items-center`}>
+        <div className={`${styles["left"]} grow`}>
+          <Button
+            className={`hover:bg-transparent cursor-pointer`}
+            size={"icon"}
+            variant={"ghost"}
+          >
+            <ChevronLeft color="white" />
+          </Button>
+        </div>
+        <div className={`${styles["center"]} grow flex justify-center`}>
+          <div className={`${styles["name"]} text-lg font-semibold`}>
+            Channel name
+          </div>
+        </div>
+        <div className={`${styles["right"]} grow flex justify-end bg-red`}>
+          <Button
+            className={`hover:bg-neutral-800 cursor-pointer`}
+            size={"icon"}
+            variant={"ghost"}
+          >
+            <Menu color="white" />
+          </Button>
+        </div>
       </div>
     </header>
   );
