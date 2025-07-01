55ea99e37d26da84f23dcd7f0d63b3ad33975fe4
nera1 <nera4936@gmail.com>
2025-07-01 23:49:11 +0900

    feat: add channel forbidden page

--- src/components/channel-forbidden/channel-forbidden.tsx
+++ src/components/channel-forbidden/channel-forbidden.tsx
@@ -1,5 +1,7 @@
 import { FunctionComponent } from "react";
 
+import Link from "next/link";
+
 import { Button } from "../ui/button";
 
 import { Search, SquarePlus } from "lucide-react";
@@ -18,12 +20,14 @@ const ChannelForbidden: FunctionComponent = () => {
           <h1 className="text-2xl tracking-tight text-balance font-semibold">
             채널에 참여할 수 없습니다
           </h1>
-          <h2 className="text-muted-foreground text-md mt-1">
+          <h2 className="text-muted-foreground text-md my-1">
             채널에 접근할 권한이 없거나, 참여할 수 없는 상태입니다
           </h2>
           <div className="flex flex-col py-3 gap-y-2">
-            <Button className="cursor-pointer">
-              <Search /> 채널 검색
+            <Button className="cursor-pointer" asChild>
+              <Link href="/channel/search">
+                <Search /> 채널 검색
+              </Link>
             </Button>
             <Button className="cursor-pointer" disabled>
               <SquarePlus /> 채널 참여
