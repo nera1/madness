a3b561f5d2789a53fe49d31771f8bfd67a1908ae
nera1 <nera4936@gmail.com>
2025-11-09 12:33:16 +0900

    feat: add editor

---
+++ src/components/Editor/index.tsx
@@ -0,0 +1,117 @@
+"use client";
+
+import React, {
+  useEffect,
+  useMemo,
+  useState,
+  useRef, // ⬅ 요거 추가
+} from "react";
+import YooptaEditor, {
+  createYooptaEditor,
+  type YooptaContentValue,
+  type YooptaOnChangeOptions,
+  type Tools,
+  type YooptaPlugin,
+  SlateElement,
+  buildBlockData,
+} from "@yoopta/editor";
+
+// Plugins
+import Paragraph from "@yoopta/paragraph";
+import Blockquote from "@yoopta/blockquote";
+import Accordion from "@yoopta/accordion";
+import Divider from "@yoopta/divider";
+import Table from "@yoopta/table";
+import Code from "@yoopta/code";
+import Embed from "@yoopta/embed";
+import Image from "@yoopta/image";
+import Link from "@yoopta/link";
+import File from "@yoopta/file";
+import Callout from "@yoopta/callout";
+import Video from "@yoopta/video";
+import Lists from "@yoopta/lists";
+import Headings from "@yoopta/headings";
+
+// Marks (Bold, Italic, ...)
+import {
+  Bold,
+  Italic,
+  CodeMark,
+  Underline,
+  Strike,
+  Highlight,
+} from "@yoopta/marks";
+
+// Tools (Toolbar, Slash(Action) Menu, Link Tool)
+import LinkTool, { DefaultLinkToolRender } from "@yoopta/link-tool";
+import ActionMenu, { DefaultActionMenuRender } from "@yoopta/action-menu-list";
+import Toolbar, { DefaultToolbarRender } from "@yoopta/toolbar";
+
+const { HeadingOne, HeadingTwo, HeadingThree } = Headings;
+const { BulletedList, NumberedList, TodoList } = Lists;
+
+const PLUGINS = [
+  Paragraph,
+  HeadingOne,
+  HeadingTwo,
+  HeadingThree,
+  BulletedList,
+  NumberedList,
+  TodoList,
+  Blockquote,
+  Code,
+  Divider,
+  Accordion,
+  Table,
+  Image,
+  Video,
+  Embed,
+  Link,
+  File,
+  Callout,
+];
+
+const MARKS = [Bold, Italic, CodeMark, Underline, Strike, Highlight];
+
+const TOOLS: Partial<Tools> = {
+  Toolbar: {
+    tool: Toolbar,
+    render: DefaultToolbarRender,
+  },
+  ActionMenu: {
+    tool: ActionMenu,
+    render: DefaultActionMenuRender,
+  },
+  LinkTool: {
+    tool: LinkTool,
+    render: DefaultLinkToolRender,
+  },
+};
+
+export default function YooptaEditorClient() {
+  const editor = useMemo(() => createYooptaEditor(), []);
+  const [value, setValue] = useState<YooptaContentValue>(() => {
+    const id = "block-1";
+    return {
+      [id]: buildBlockData({ id }), // 기본 paragraph block
+    };
+  });
+
+  return (
+    <div className="flex w-full justify-center px-5 py-6">
+      <YooptaEditor
+        editor={editor}
+        plugins={
+          PLUGINS as unknown as YooptaPlugin<Record<string, SlateElement>>[]
+        }
+        marks={MARKS}
+        tools={TOOLS}
+        value={value}
+        autoFocus
+        placeholder=""
+        className="yoopta-editor prose dark:prose-invert max-w-none"
+        style={{ width: "640px" }}
+      />
+    </div>
+  );
+}
