875fec45529d4ddbcd44b06c782c2199fab47a6b
nera1 <nera4936@gmail.com>
2025-12-01 23:34:03 +0900

    styles: shrink side padding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import VerticalToolbar from "@/components/vertical-toolbar/vertical-toolbar";
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full items-center justify-center py-32 px-16 bg-white dark:bg-black">
+      <main className="flex min-h-screen w-full items-center justify-center pt-24 pb-32 px-12 bg-white dark:bg-black">
         <VerticalToolbar />
         <Editor />
       </main>
